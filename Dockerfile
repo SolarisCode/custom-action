6bb0dc1d028e037129fc29659f72f32291e5eaa8
melkholy <28461793+SolarisCode@users.noreply.github.com>
2024-02-25 21:29:56 +0100

    Entrypoint script and metadata yml file were created

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM ubuntu:focal
 
 RUN apt-get update \
 	&& apt-get install clang-format \
-	vim -y
+	httpie \
+	jq -y
 
-WORKDIR /app
-COPY ./main.cpp ./entrypoint.sh /app/
-ENTRYPOINT [ "./entrypoint.sh" ]
+COPY ./entrypoint.sh /usr/local/bin/
+ENTRYPOINT [ "entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,70 @@
 #!/bin/bash
 
-clang-format -Werror --dry-run --style=webkit ./*.cp*
+# Check Cpp files for lintting errors
+check_lintting_errors()
+{
+	clang-format -Werror --dry-run --style=webkit ./*.cpp ./*/*.cpp 2&> errors.log
+	if [[ $(wc -l < errors.log) == 0 ]];
+	then
+		echo "No lintting errors were found!"
+		exit 0
+	fi
+}
+
+# Push the found lintting erros to the repo if there are any
+push_lintting_errors()
+{
+	check_lintting_errors
+
+	# Convert the lintting error file to base64 to push to Github
+	ERRORS=$( base64 < errors.log )
+
+	echo "Pushing lintting errors to the repo......"
+	# Using httpie and Github APIs tp push the lintting error file to the repo
+	http PUT https://api.github.com/repos/"$GITHUB_REPOSITORY"/contents/errors.log \
+		"Authorization: Bearer $GITHUB_TOKEN" \
+		message="Lintting errors were detected!" \
+		committer:="{ \"name\": \"$GITHUB_TOKEN\", \"email\": \"$GITHUB_ACTOR@github.com\" }" \
+		content="$ERRORS" | jq .
+}
+
+# Fix all the lintting errors inplace if the "FIXIT" keyword mentioned in the commit message
+fix_lintting_errors()
+{
+	check_lintting_errors
+
+	# Fix all the errors inplace using -i option
+	if clang-format -Werror -i --style=webkit ./*.cpp ./*/*.cpp;
+	then
+		echo "All errors were resolved"
+	else
+		echo "Not all errors were resolved, please double check!"
+		exit 1
+	fi
+}
+
+check_arguments()
+{
+	if jq '.commits[].message, .head-commit.message' < "$GITHUB_EVENT_PATH" | grep -iq "$*";
+	then
+		echo "$* Keyword argument was found!"
+		fix_lintting_errors
+	else
+		echo "$* Keyword argument was not found!"
+		exit 1
+	fi
+}
+
+if [ -z "$GITHUB_EVENT_PATH" ];
+then
+	echo "Something went wrong!"
+	exit 1
+fi
+
+if [ -n "$*" ];
+then
+	check_arguments "$@"
+else
+	echo "Check Cpp files for lintting erros......"
+	push_lintting_errors
+fi
